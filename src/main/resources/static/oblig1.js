c06451f3b98fc98a35ac195292fe1ff536ff3751
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-02-13 11:39:53 +0100

    Bygge opp html js

---
+++ src/main/resources/static/oblig1.js
@@ -0,0 +1,13 @@
+const ticketList = [10];
+let fornavn =
+
+function registrer(){
+    ticketList.push()
+
+}
+
+function skrivUt(){
+    for(let i = 0; i < ticketList.length; i++){
+        document.write([i]+"\n");
+    }
+}
