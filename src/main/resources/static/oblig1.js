e1a4a50b92ce2ece41e1f1fd58d9c9719c44f875
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-02-13 13:57:24 +0100

    lagt til nedtrekksmeny

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,13 +1,31 @@
+
 const ticketList = [10];
-let fornavn =
 
-function registrer(){
-    ticketList.push()
 
+const person = {
+
+    antall : document.getElementById("antall").value,
+    fornavn : document.getElementById("fornavn").value,
+    etternavn : document.getElementById("etternavn").value,
+    epost : document.getElementById("epost").value,
+};
+
+function registrer() {
+    for (let i = 0; i < ticketList.length; i++) {
+        ticketList.push(person.antall);
+        ticketList.push(person.fornavn);
+        ticketList.push(person.etternavn);
+        ticketList.push(person.epost);
+
+        console.log("Nå har vi registrert litt ting.");
+    }
 }
 
 function skrivUt(){
+    let tabell = "<table><tr>" + "<th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Epost</th></tr></table>";
+
     for(let i = 0; i < ticketList.length; i++){
-        document.write([i]+"\n");
+       tabell += "<tr><td>"+ ticketList[i].antall +"</td><td>"+ person[i].fornavn + "</td>" +
+           "<td>"+ person[i].etternavn +"</td><td>"+person[i].epost+"</td><br/>";
     }
 }
