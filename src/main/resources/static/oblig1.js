fdc8c5c3ed596cd7f6de79d840e95c3963f9a6ad
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-02-13 16:10:28 +0100

    Lagt inn validering av tomme inputfelt

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -4,27 +4,60 @@ const ticketList = [10];
 
 
 function registrer() {
-    const person = {
 
-        antall : document.getElementById("antall").value,
-        fornavn : document.getElementById("fornavn").value,
-        etternavn : document.getElementById("etternavn").value,
-        epost : document.getElementById("epost").value,
-    };
+    if (
+        document.getElementById("film").value === null ||
+        document.getElementById("antall").value === null ||
+        document.getElementById("fornavn").value === null ||
+        document.getElementById("etternavn").value === null ||
+        document.getElementById("telefonnr").value === null ||
+        document.getElementById("epost").value === null) {
 
-    for (let i = 0; i < ticketList.length; i++) {
-        ticketList.push(person.antall);
-        ticketList.push(person.fornavn);
-        ticketList.push(person.etternavn);
-        ticketList.push(person.epost);
+        let errorMelding = "Skriv inn i manglende felt.";
 
-        console.log("Nå har vi registrert litt ting.");
+        document.getElementById("antallId").innerHTML = errorMelding;
+        document.getElementById("fornavnId").innerHTML = errorMelding;
+        document.getElementById("etternavnId").innerHTML = errorMelding;
+        document.getElementById("telefonnrId").innerHTML = errorMelding;
+        document.getElementById("epostId").innerHTML = errorMelding;
+
+    } else {
+        const person = {
+
+            film: document.getElementById("film").value,
+            antall: document.getElementById("antall").value,
+            fornavn: document.getElementById("fornavn").value,
+            etternavn: document.getElementById("etternavn").value,
+            telefonnr: document.getElementById("telefonnr").value,
+            epost: document.getElementById("epost").value,
+
+        };
+
+        for (let i = 0; i < ticketList.length; i++) {
+            ticketList.push(person.film);
+            ticketList.push(person.antall);
+            ticketList.push(person.fornavn);
+            ticketList.push(person.etternavn);
+            ticketList.push(person.epost);
+
+            console.log("Nå har vi registrert litt ting.");
+        }
+        let tabell = "<table><tr>" + "<th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Epost</th></tr></table>";
+
+        for (let i = 0; i < ticketList.length; i++) {
+            tabell += "<tr><td>" + ticketList[i].film + "</td><td>" + ticketList[i].antall + "</td><td>" + ticketList[i].fornavn + "</td>" +
+                "<td>" + ticketList[i].etternavn + "</td><td>" + ticketList[i].epost + "</td><br/>";
+        }
+        document.getElementById("textfield").innerHTML = tabell;
+
+        /*Setter inputfeltene blanke:*/
+        document.getElementById("antall").innerHTML = "";
+        document.getElementById("fornavn").innerHTML = "";
+        document.getElementById("etternavn").innerHTML = "";
+        document.getElementById("epost").innerHTML = "";
     }
-    let tabell = "<table><tr>" + "<th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Epost</th></tr></table>";
-
-    for(let i = 0; i < ticketList.length; i++){
-        tabell += "<tr><td>"+ ticketList[i].antall +"</td><td>"+ person[i].fornavn + "</td>" +
-            "<td>"+ person[i].etternavn +"</td><td>"+person[i].epost+"</td><br/>";
-    }
-    document.getElementById("textfield").innerHTML = tabell;
 }
+
+/*function slettArray(){
+
+}*/
