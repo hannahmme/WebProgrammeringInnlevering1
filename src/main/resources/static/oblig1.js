c0d86cb11fc7e99c232e227cc1990f4d52de7673
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-02-13 14:23:49 +0100

    lagt til nedtrekksmeny

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -2,15 +2,16 @@
 const ticketList = [10];
 
 
-const person = {
-
-    antall : document.getElementById("antall").value,
-    fornavn : document.getElementById("fornavn").value,
-    etternavn : document.getElementById("etternavn").value,
-    epost : document.getElementById("epost").value,
-};
 
 function registrer() {
+    const person = {
+
+        antall : document.getElementById("antall").value,
+        fornavn : document.getElementById("fornavn").value,
+        etternavn : document.getElementById("etternavn").value,
+        epost : document.getElementById("epost").value,
+    };
+
     for (let i = 0; i < ticketList.length; i++) {
         ticketList.push(person.antall);
         ticketList.push(person.fornavn);
@@ -19,13 +20,11 @@ function registrer() {
 
         console.log("Nå har vi registrert litt ting.");
     }
-}
-
-function skrivUt(){
     let tabell = "<table><tr>" + "<th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Epost</th></tr></table>";
 
     for(let i = 0; i < ticketList.length; i++){
-       tabell += "<tr><td>"+ ticketList[i].antall +"</td><td>"+ person[i].fornavn + "</td>" +
-           "<td>"+ person[i].etternavn +"</td><td>"+person[i].epost+"</td><br/>";
+        tabell += "<tr><td>"+ ticketList[i].antall +"</td><td>"+ person[i].fornavn + "</td>" +
+            "<td>"+ person[i].etternavn +"</td><td>"+person[i].epost+"</td><br/>";
     }
+    document.getElementById("textfield").innerHTML = tabell;
 }
