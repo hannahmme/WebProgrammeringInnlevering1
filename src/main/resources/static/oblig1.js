488bc3f5f9382b327a6eaa55a4fd33c7035f8c52
Hannah M.M. Eriksen <hannah_eriksen@hotmail.com>
2020-02-17 20:04:02 +0100

    Endelig fått ordnet onchange for errorMeldinger

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -3,6 +3,13 @@ const ticketList = [];
 
 
 
+//Funksjon som skal sende inn elementId som parameter, som skal sette feltene til null.
+function deleteMsg(elementId){
+    document.getElementById(elementId).innerHTML = "";
+
+}
+
+
 function registrer() {
 
     let mangelMld = "Skriv inn i manglende felt.";
@@ -19,13 +26,13 @@ function registrer() {
 
 
     //Div-feltene feilmeldinger skal skrives ut i.
-    let divFilm = document.getElementById("filmId").innerHTML;
+ /*   let divFilm = document.getElementById("filmId").innerHTML;
     let divAntall = document.getElementById("antallId").innerHTML;
     let divFornavn = document.getElementById("fornavnId").innerHTML;
     let divEtternavn = document.getElementById("etternavnId").innerHTML;
     let divTlf = document.getElementById("telefonnrId").innerHTML;
     let divEpost = document.getElementById("epostId").innerHTML;
-
+*/
 
     /*   try {*/
     let quantity = document.getElementById("antall").value;
@@ -35,32 +42,39 @@ function registrer() {
     let tlfNumb = Number(tlf);
 
 
+    let isValid = true;
+    //Bool som sier om vi går inn i if-ene. Gjør den det, er den false og
+    //vil ikke la oss legge inn person i array.
+
     //Sjekker om inputfeltene ikke er tomme eller NaN. (Not a number).
     if (film.length === 0) {
         document.getElementById("filmId").innerHTML = mangelMld;
-    }  else if (antall.length === 0 || Number.isNaN(antNumb)) {
-        console.log(antNumb + "Dette er antNumb");
-        console.log(antall.length + "Dette er lengden på antall.");
+        isValid = false;
 
-
+    } if (antall.length === 0 || Number.isNaN(antNumb)) {
         document.getElementById("antallId").innerHTML = notNumbMsg;
-    } else if (fornavn.length === 0) {
-        document.getElementById("fornavnId").innerHTML = mangelMld;
-    } else if (etternavn.length === 0) {
-        document.getElementById("etternavnId").innerHTML = mangelMld;
-    } else if (telefon.length === 0 || Number.isNaN(tlfNumb)) {
-        document.getElementById("telefonnrId").innerHTML = notNumbMsg;
-    } else if (epost.length === 0) {
-        document.getElementById("epostId").innerHTML = mangelMld;
-    }
-        //Sjekker om input faktisk er tall.
-     /*else if (
-            !isNaN(antNumb)){
-            document.getElementById("antallId").innerHTML = notNumbMsg;
-            console.log("Nå er vi i antNumber sin if");
+        isValid = false;
 
-            //Hvis alt er riktig, opprett person og legg inn i array.
-        } */ else {
+    } if (fornavn.length === 0) {
+        document.getElementById("fornavnId").innerHTML = mangelMld;
+        isValid = false;
+
+    } if (etternavn.length === 0) {
+        document.getElementById("etternavnId").innerHTML = mangelMld;
+        isValid = false;
+
+    } if (telefon.length === 0 || Number.isNaN(tlfNumb)) {
+        document.getElementById("telefonnrId").innerHTML = notNumbMsg;
+        isValid = false;
+
+    } if (epost.length === 0) {
+        document.getElementById("epostId").innerHTML = mangelMld;
+        isValid = false;
+    }
+
+
+    //Hvis alt er riktig, opprett person og legg inn i array.
+    if(isValid){
         const person = {
 
             film: document.getElementById("film").value,
@@ -76,14 +90,33 @@ function registrer() {
         console.log(ticketList.length + " = her er lengden.");
 
 
-        let tabell = "<table><tr>" + " <th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>Epost</th>" + "</tr>";
+        let tabell = "<table><tr>" + "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>Epost</th>" + "</tr>";
 
         for (let i = 0; i < ticketList.length; i++) {
             tabell += "<tr><td>" + ticketList[i].film + "</td><td>" + ticketList[i].antall + "</td><td>" + ticketList[i].fornavn + "</td>" +
-                "<td>" + ticketList[i].etternavn + "</td><td>" + ticketList[i].telefonnr + "</td><td>" + ticketList[i].epost + "</td><br/>";
+                "<td>" + ticketList[i].etternavn + "</td><td>" + ticketList[i].telefonnr + "</td><td>" + ticketList[i].epost + "</td>";
         }
         document.getElementById("textfield").innerHTML = tabell;
         tabell += "</table>";
+
+
+        //fjerner inputverdier når en trykker registrer.
+        document.getElementById("film").value = "Velg film";
+        document.getElementById("antall").value = "";
+        document.getElementById("fornavn").value = "";
+        document.getElementById("etternavn").value = "";
+        document.getElementById("telefonnr").value = "";
+        document.getElementById("epost").value = "";
+
+        document.getElementById("filmId").innerHTML = "";
+        document.getElementById("antallId").innerHTML = "";
+        document.getElementById("fornavnId").innerHTML = "";
+        document.getElementById("etternavnId").innerHTML = "";
+        document.getElementById("telefonnrId").innerHTML = "";
+        document.getElementById("epostId").innerHTML = "";
+
+
+
 
 
 
